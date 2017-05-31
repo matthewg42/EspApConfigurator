5b0a3b64ca2b22f38ac0d63f9567e8277ca8fce4
Matthew Gates <matthewg42@gmail.com>
2017-05-31 11:33:51 +0100

    add a lot of comments

--- 06_mine1/ModeAPServe.cpp
+++ 06_mine1/ModeAPServe.cpp
@@ -22,27 +22,37 @@ ModeAPServe_::ModeAPServe_() :
 void ModeAPServe_::modeStart()
 {
     DBLN(F("S:ModeAPServe::modeStart()"));
+    
+    // Change heartbeat to indicate AP mode
+    HeartBeat.setCustomMode(900, 100);
+
+    // Make sure the WiFi is turned on
+    WiFi.forceSleepWake();
+
+    // This is required for AP mode to work properly with my setup
+    // (NodeMCU)
+    WiFi.mode(WIFI_STA);
+
+    // Put into AP mode, with specified IP/netmask
+    WiFi.softAPConfig(apIP, apIP, netMsk);
+
+    // Set AP SSID and passphrase
+    WiFi.softAP(AP_NAME, AP_PASS);
+
+    DB(" AP IP address: ");
+    DBLN(WiFi.softAPIP());
+
+    // Set up DNS resolver for all names going to local IP
     if (!pDnsServer) {
         pDnsServer = new DNSServer();
     }
-    if (!pHttpServer) {
-        pHttpServer = new ESP8266WebServer(80);
-    }
-    
-    HeartBeat.setMode(Heartbeat::Quick);
-    // Make sure the WiFi is turned on
-    WiFi.forceSleepWake();
-    WiFi.mode(WIFI_STA);
-    delay(500);
-    WiFi.softAPConfig(apIP, apIP, netMsk);
-    WiFi.softAP(AP_NAME, AP_PASS);
-    delay(300);
-    DB(" AP IP address: ");
-    DBLN(WiFi.softAPIP());
     pDnsServer->setErrorReplyCode(DNSReplyCode::NoError);
     pDnsServer->start(53, "*", WiFi.softAPIP());
 
     // Set up routes for web server
+    if (!pHttpServer) {
+        pHttpServer = new ESP8266WebServer(80);
+    }
     pHttpServer->on("/", handleRoot);
     pHttpServer->onNotFound(handleNotFound);
     pHttpServer->begin();
@@ -52,11 +62,14 @@ void ModeAPServe_::modeStart()
 
 void ModeAPServe_::modeEnd()
 {
+    DBLN("ModeAPServe::modeEnd");
+    // destroy DNS to save memory when not in AP mode
     if (pDnsServer) {
         delete pDnsServer;
         pDnsServer = NULL;
     }
 
+    // destroy HTTP server to save memory when not in AP mode
     if (pHttpServer) {
         delete pHttpServer;
         pHttpServer = NULL;
@@ -65,6 +78,7 @@ void ModeAPServe_::modeEnd()
 
 void ModeAPServe_::modeUpdate()
 {
+    // Handle incoming connections
     pDnsServer->processNextRequest();
     pHttpServer->handleClient();
 }
