ca4cd779aad667073acf939c2c05f2388bc70729
Matthew Gates <matthewg42@gmail.com>
2017-06-04 14:41:26 +0100

    remove all that printf debugging

--- PersistentSettingString.cpp
+++ PersistentSettingString.cpp
@@ -1,5 +1,4 @@
 #include <EEPROM.h>
-#include <MutilaDebug.h>
 #include "PersistentSettingString.h"
 
 bool basicStringValidator(String s)
@@ -62,9 +61,6 @@ bool PersistentSettingString::load()
 
 bool PersistentSettingString::save()
 {
-    DB(F("PersistentSettingString::save, value=\""));
-    DB(_value);
-    DBLN('\n');
     for (uint16_t offset=0; offset<_maxLength; offset++) {
         uint8_t c;
         if (offset < _value.length()) {
@@ -72,19 +68,11 @@ bool PersistentSettingString::save()
         } else {
             c = 0;
         }
-        DB(F("address="));  DB(_address);
-        DB(F(" offset="));  DB(offset);
-        DB(F(" c="));       DB(c);
-        DB(F(" ("));        DB((int)c);     DBLN(')');
         EEPROM.write(_address+offset, c);
     }
     EEPROM.commit();
     String saved = peek();
-    DB(F("saved=\""));
-    DB(saved);
     bool ok = (saved==_value);
-    DB(F("\" ok="));
-    DBLN(ok);
     return ok;
 }
 
@@ -95,23 +83,15 @@ uint16_t PersistentSettingString::size()
 
 String PersistentSettingString::peek()
 {
-    DBLN(F("PersistentSettingString::peek"));
     String loaded;
     for (uint16_t offset=0; offset<_maxLength; offset++) {
         uint8_t c = EEPROM.read(_address+offset);
-        DB(F("address="));  DB(_address);
-        DB(F(" offset="));  DB(offset);
-        DB(F(" c="));       DB(c);
-        DB(F(" ("));        DB((int)c); DBLN(')');
         if (c != 0) {
             loaded += (char)c;
         } else {
             break;
         }
     }
-    DB(F("peek loaded=\""));
-    DB(loaded);
-    DBLN('\"');
     return loaded;
 }
 
