c3ed788e0724f7630d4098938124a52ad1701901
Matthew Gates <matthewg42@gmail.com>
2017-06-10 18:52:47 +0100

    force valid bool setting

--- PersistentSettingBool.cpp
+++ PersistentSettingBool.cpp
@@ -1,5 +1,11 @@
 #include <EEPROM.h>
+#include <MutilaDebug.h>
 #include "PersistentSettingBool.h"
+
+bool basicBoolValidator(String s)
+{
+    return s == "0" || s == "1";
+}
 
 PersistentSettingBool::PersistentSettingBool(uint16_t eepromAddress, bool defaultValue, validatorFunction validator) :
     PersistentSettingAtom<bool>(eepromAddress, defaultValue, validator)
--- PersistentSettingBool.h
+++ PersistentSettingBool.h
@@ -4,6 +4,8 @@
 #include <Arduino.h>
 #include "PersistentSettingAtom.h"
 
+bool basicBoolValidator(String s);
+
 /*! \brief Class for storing boolean values to EEPROM
  *
  */
@@ -14,7 +16,7 @@ public:
      * \param defaultValue value to take before loading
      * \param validatorFunction function to validate values
      */
-    PersistentSettingBool(uint16_t eepromAddress, bool defaultValue, validatorFunction validator=NULL);
+    PersistentSettingBool(uint16_t eepromAddress, bool defaultValue, validatorFunction validator=&basicBoolValidator);
 
     /*! Load from EEPROM 
      *  Loads value from EEPROM, and checks for validity. If valid use value and return
@@ -27,9 +29,8 @@ public:
      *
      *  If newValue is valid, set it and return true, else return false.
      */
-    bool set(String newValue);
-
-    String typecode() { return String("b"); }
+    bool set(String newValue) override;
+    String typecode() override { return String("b"); }
 
 };
 
